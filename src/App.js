0f83e4a80a3065acb1c835a3b35f7e591baa4b34
CojaONIX <cojaonix@gmail.com>
2024-04-15 13:59:29 +0200

    1.16 style solution

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './style.css';
 
-const NAME = 'Goran';
+const name = 'Goran';
 const myStyle = {
   color: 'blue',
   backgroundColor: 'yellow',
   padding: '10px'
 };
 
+const paragraphColor = 'red';
+
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="logo" alt="logo" />
+        <img src={ logo } className="logo" alt="logo" />
         <h1>Hello world</h1>
-        <p>Pozdrav. Ja se zovem {NAME}!</p>
-        <p style={myStyle}>Ovo je moj prvi React App</p>
+        <p>Pozdrav. Ja se zovem { name }!</p>
+        <p style={ myStyle }>Ovo je moj prvi React App</p>
+        <p style={{ color: paragraphColor, backgroundColor: 'yellow', padding: '10px' }}>Ovo je moj prvi React App</p>
     </div>
   );
 }
