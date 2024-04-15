3799bc80363370d3bc0bae0f1d04469d4d5f181f
CojaONIX <cojaonix@gmail.com>
2024-04-15 17:38:22 +0200

    2.5 Products map

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,8 +1,21 @@
 
+let products = [
+    'iPhone 14', 'iPhone 15', 'Samsung S23'
+];
+
 function Products () {
     return (
-        <h1>Hello world</h1>
-    )
+        <>
+            <h1>Hello world</h1>
+            <ul>
+            {
+                products.map(function(product) {
+                    return <li>{product}</li>
+                })
+            }
+            </ul>
+        </>
+    );
 };
 
 export default Products;
